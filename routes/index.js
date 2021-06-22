a6f2050f0a97ff4860d2f6ffde670f8f37a868f7
aris.tsakpinis@ibm.com <aris.tsakpinis@ibm.com>
2021-06-22 11:09:48 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Next' });
+  res.render('index', { title: 'Express Next2' });
 });
 
 module.exports = router;
